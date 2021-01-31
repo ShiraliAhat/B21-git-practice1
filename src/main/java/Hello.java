ca721f6a84edf227c1876b86cf265c44bc1c2e8a
Shirali-Ahat <shirali0215@gmail.com>
2021-01-31 00:20:36 -0800

    Master branch  , spouse changed movie location

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        System.out.println("hello wrld b22");
         System.out.println("B21 is the best");
 
 
