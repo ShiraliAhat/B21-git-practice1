928d60773e6facd35a741406464a98051aa7effc
Shirali-Ahat <shirali0215@gmail.com>
2021-01-30 23:35:23 -0800

    B21 is learning more git

--- src/test/java/featureACode.java
+++ src/test/java/featureACode.java
@@ -2,5 +2,6 @@ public class featureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A starded");
+        System.out.println("B21 is learning more git with review");
     }
 }
